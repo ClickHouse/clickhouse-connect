c51be7ea0fae8d49af4b82129efe12941cf45280
Geoff Genz <geoff@clickhouse.com>
2022-06-11 17:04:07 -0600

    Add validation utility for entrypoints

--- clickhouse_connect/__init__.py
+++ clickhouse_connect/__init__.py
@@ -1,7 +1,12 @@
 from clickhouse_connect.driver import create_client
+from clickhouse_connect.entry_points import validate_entrypoints
 
 driver_name = 'clickhousedb'
 
 
 def get_client(**kwargs):
     return create_client(**kwargs)
+
+
+def check_ep():
+    assert validate_entrypoints() == 0
