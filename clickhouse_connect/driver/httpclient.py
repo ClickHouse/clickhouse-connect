9b901569d7dba0f23e1f4764263c41b72b7a3e82
Geoff Genz <geoff@clickhouse.com>
2022-06-26 15:15:34 -0600

    Fix lint

--- clickhouse_connect/driver/httpclient.py
+++ clickhouse_connect/driver/httpclient.py
@@ -33,8 +33,7 @@ PyHttp._MAXHEADERS = 10000  # pylint: disable=protected-access
 # pylint: disable=too-many-instance-attributes
 class HttpClient(Client):
     valid_transport_settings = {'database', 'buffer_size', 'session_id', 'compress', 'decompress',
-                          'session_timeout', 'session_id', 'session_check', 'query_id',
-                          'quota_key'}
+                                'session_timeout', 'session_check', 'query_id', 'quota_key'}
 
     # pylint: disable=too-many-arguments,too-many-locals,too-many-branches,too-many-statements
     def __init__(self, interface: str, host: str, port: int, username: str, password: str, database: str,
