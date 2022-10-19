b2a768b2a8bcc8567c5c005050e366a51eb57896
Geoff Genz <geoff@clickhouse.com>
2022-10-19 08:58:39 -0600

    Type fixes (#58)
    * Fix UInt64 type bug, fix Superset big integers
    
    * Fix typos
    
    * Fix lint

---
+++ tests/integration_tests/test_formats.py
@@ -0,0 +1,15 @@
+from clickhouse_connect.driver import Client, ProgrammingError
+
+
+def test_uint64_format(test_client: Client):
+    # Default should be unsigned
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value')
+    assert result.result_set[0][0] == 9523372036854775807
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value', query_formats={'UInt64': 'signed'})
+    assert result.result_set[0][0] == -8923372036854775809
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value', query_formats={'UInt64': 'native'})
+    assert result.result_set[0][0] == 9523372036854775807
+    try:
+        test_client.query('SELECT 9523372036854775807::UInt64 as signed', query_formats={'UInt64': 'huh'})
+    except ProgrammingError:
+        pass
