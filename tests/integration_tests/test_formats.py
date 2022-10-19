8bff9adb3a1c313025002a924b03e669697259a6
Geoff Genz <geoff@clickhouse.com>
2022-10-19 08:47:31 -0600

    Fix lint

--- tests/integration_tests/test_formats.py
+++ tests/integration_tests/test_formats.py
@@ -3,15 +3,13 @@ from clickhouse_connect.driver import Client, ProgrammingError
 
 def test_uint64_format(test_client: Client):
     # Default should be unsigned
-    rs = test_client.query("SELECT 9523372036854775807::UInt64 as value")
-    assert rs.result_set[0][0] == 9523372036854775807
-    rs = test_client.query("SELECT 9523372036854775807::UInt64 as value", query_formats={'UInt64': 'signed'})
-    assert rs.result_set[0][0] == -8923372036854775809
-    rs = test_client.query("SELECT 9523372036854775807::UInt64 as value", query_formats={'UInt64': 'native'})
-    assert rs.result_set[0][0] == 9523372036854775807
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value')
+    assert result.result_set[0][0] == 9523372036854775807
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value', query_formats={'UInt64': 'signed'})
+    assert result.result_set[0][0] == -8923372036854775809
+    result = test_client.query('SELECT 9523372036854775807::UInt64 as value', query_formats={'UInt64': 'native'})
+    assert result.result_set[0][0] == 9523372036854775807
     try:
-        rs = test_client.query("SELECT 9523372036854775807::UInt64 as signed", query_formats={'UInt64': 'huh'})
+        test_client.query('SELECT 9523372036854775807::UInt64 as signed', query_formats={'UInt64': 'huh'})
     except ProgrammingError:
         pass
-
-
