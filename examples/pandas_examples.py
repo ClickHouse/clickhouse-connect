c3abfe54604ca891f908cadf20b0479f010d55d2
Geoff Genz <geoff@clickhouse.com>
2022-11-02 11:05:44 -0600

    Fix query_context.updated_copy method, add first examples (#66)

---
+++ examples/pandas_examples.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import clickhouse_connect
+
+
+create_table_sql = """
+CREATE TABLE pandas_example
+(
+    `timeseries` DateTime('UTC'),
+    `int_value` Int32,
+    `str_value` String,
+    `float_value` Float64
+)
+ENGINE = MergeTree
+ORDER BY timeseries
+"""
+
+
+def write_pandas_df():
+    client = clickhouse_connect.get_client(host='localhost', port='8123', user_name='default', password = '')
+    client.command('DROP TABLE IF EXISTS pandas_example')
+    client.command(create_table_sql)
+    df = pd.DataFrame({'timeseries': ['04/03/2022 10:00:11', '05/03/2022 11:15:44', '06/03/2022 17:14:00'],
+                      'int_value': [16, 19, 11],
+                       'str_value': ['String One', 'String Two', 'A Third String'],
+                       'float_value': [2344.288, -73002.4444, 3.14159]})
+    df['timeseries'] = pd.to_datetime(df['timeseries'])
+    client.insert_df('pandas_example', df)
+    result_df = client.query_df('SELECT * FROM pandas_example')
+    print()
+    print(result_df.dtypes)
+    print()
+    print(result_df)
+
+
+if __name__ == '__main__':
+    write_pandas_df()
