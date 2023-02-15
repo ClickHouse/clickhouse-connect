6af9017bf7b67bd791dcc26232be01950230b2b4
Geoff Genz <geoff@clickhouse.com>
2023-02-15 14:19:48 -0700

    tweak test for Python 3.7

--- tests/integration_tests/test_protocol_version.py
+++ tests/integration_tests/test_protocol_version.py
@@ -4,7 +4,7 @@ from clickhouse_connect.driver import Client
 def test_protocol_version(test_client: Client):
     query = "select toDateTime(1676369730, 'Asia/Shanghai') as dt FORMAT Native"
     raw = test_client.raw_query(query)
-    assert raw.hex(' ', 2) == '0101 0264 7408 4461 7465 5469 6d65 425f eb63'
+    assert raw.hex() == '0101026474084461746554696d65425feb63'
 
     if test_client.min_version('23.3'):
         raw = test_client.raw_query(query, settings={'client_protocol_version': 54337})
