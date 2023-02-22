80c197cd4ee26ee8d8b6e44188631d2f8bea82d4
Geoff Genz <geoff@clickhouse.com>
2023-02-21 21:10:58 -0700

    Fix lint

--- tests/integration_tests/test_client.py
+++ tests/integration_tests/test_client.py
@@ -56,7 +56,7 @@ def test_none_database(test_client: Client):
 
 
 def test_insert(test_client: Client, test_table_engine: str):
-    test_client.command(f'DROP TABLE IF EXISTS test_system_insert SYNC')
+    test_client.command('DROP TABLE IF EXISTS test_system_insert SYNC')
     test_client.command(f'CREATE TABLE test_system_insert AS system.tables Engine {test_table_engine} ORDER BY name')
     tables_result = test_client.query('SELECT * from system.tables')
     test_client.insert(table='test_system_insert', column_names='*', data=tables_result.result_set)
