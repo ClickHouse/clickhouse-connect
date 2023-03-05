254b4d8729fbe84c8eb4460cfce5097ef11f83c1
Aleksey Astafiev <26490932+aastafiev@users.noreply.github.com>
2023-03-05 22:01:55 +0300

    revert back playtest

--- playtest.py
+++ playtest.py
@@ -1,35 +1,17 @@
-from io import BytesIO
-
-import pyarrow.parquet as pq
-
 import clickhouse_connect
-from clickhouse_connect import common
 
 
 def main():
-    print(f'\nClickHouse Connect installed version: {common.version()}')
-    common.set_setting('readonly', 1)
-    client = clickhouse_connect.get_client(host='localhost',
-                                           user='default',
-                                           port=9123)
+    print(f'\nClickHouse Connect installed version: {clickhouse_connect.version()}')
+    client = clickhouse_connect.get_client(host='play.clickhouse.com',
+                                           username='play',
+                                           password='clickhouse',
+                                           port=443)
     print(f'ClickHouse Play current version and timezone: {client.server_version} ({client.server_tz})')
-    Q = 'select 1000 as value'
-    # result = client.query_df(Q)
-    result = client.raw_query(Q, fmt='Parquet')
-    with BytesIO(result) as b_in:
-        table = pq.read_table(
-            source=b_in,
-            use_threads=True,
-            memory_map=True,
-            buffer_size=10000,
-        )
-    # print(table.to_pandas(split_blocks=True, self_destruct=True))
-    print(table.to_pandas())
-    # print(result)
-    # result = client.query('SHOW DATABASES')
-    # print('ClickHouse play Databases:')
-    # for row in result.result_set:
-    #     print(f'  {row[0]}')
+    result = client.query('SHOW DATABASES')
+    print('ClickHouse play Databases:')
+    for row in result.result_set:
+        print(f'  {row[0]}')
     client.close()
 
 
