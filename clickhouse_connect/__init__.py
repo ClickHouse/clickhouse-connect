4061d21a85295d77f72d351b4c58f8ddda1b9457
Geoff Genz <geoff@clickhouse.com>
2023-09-22 10:11:53 -0600

    Release 0 6 14 (#246)
    * Fix FixedString padding of empty string
    
    * Remove bad 3.7 import
    
    * Update changelog

--- clickhouse_connect/__init__.py
+++ clickhouse_connect/__init__.py
@@ -1,12 +1,8 @@
 from clickhouse_connect.driver import create_client
-from clickhouse_connect.entry_points import validate_entrypoints
+
 
 driver_name = 'clickhousedb'
 
 
 def get_client(**kwargs):
     return create_client(**kwargs)
-
-
-def check_ep():
-    assert validate_entrypoints() == 0
