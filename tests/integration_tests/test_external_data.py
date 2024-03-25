acca0c5a263fbd0679c494c069754c1ec356d2f7
Geoff Genz <geoff@clickhouse.com>
2024-03-25 12:27:30 -0600

    Adjustments for SMT cloud tests

--- tests/integration_tests/test_external_data.py
+++ tests/integration_tests/test_external_data.py
@@ -5,6 +5,7 @@ import pytest
 from clickhouse_connect.driver import Client
 from clickhouse_connect.driver.external import ExternalData
 from clickhouse_connect.driver.options import arrow
+from tests.integration_tests.conftest import TestConfig
 
 ext_settings = {'input_format_allow_errors_num': 10, 'input_format_allow_errors_ratio': .2}
 
@@ -50,7 +51,9 @@ def test_external_multiple(test_client: Client):
     assert result[0][1] == 'Scarface'
 
 
-def test_external_parquet(test_client: Client):
+def test_external_parquet(test_config: TestConfig, test_client: Client):
+    if test_config.cloud:
+        pytest.skip('External data join not working in SMT, skipping')
     movies_file = f'{Path(__file__).parent}/movies.parquet'
     test_client.command("""
 CREATE TABLE IF NOT EXISTS num (number UInt64, t String)
