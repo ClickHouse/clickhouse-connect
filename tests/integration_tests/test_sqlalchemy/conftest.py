718a0d0bcd04dc8024081a1ab1904be80373d4d9
Joe S <joe.spadola@clickhouse.com>
2026-01-30 16:27:02 -0800

    set ddl task to async mode

--- tests/integration_tests/test_sqlalchemy/conftest.py
+++ tests/integration_tests/test_sqlalchemy/conftest.py
@@ -14,7 +14,8 @@ def test_engine_fixture(test_config: TestConfig) -> Iterator[Engine]:
     test_engine: Engine = create_engine(
         f'clickhousedb://{test_config.username}:{test_config.password}@{test_config.host}:' +
         f'{test_config.port}/{test_config.test_database}?ch_http_max_field_name_size=99999' +
-        '&use_skip_indexes=0&ca_cert=certifi&query_limit=2333&compression=zstd&select_sequential_consistency=1'
+        '&use_skip_indexes=0&ca_cert=certifi&query_limit=2333&compression=zstd&select_sequential_consistency=1' +
+        '&distributed_ddl_task_timeout=0'
     )
 
     yield test_engine
