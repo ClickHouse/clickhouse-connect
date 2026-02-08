9a2cbf66baf85c95883a01c3f7fd6a6bb0be2930
Varun Chawla <varun_6april@hotmail.com>
2026-02-07 23:20:47 -0800

    Fix percent sign double encoding in SQLAlchemy string literals
    Resolves #297
    
    Previously, percent signs in SQL string literals were incorrectly
    doubled when using the SQLAlchemy interface. This broke functions
    like formatDateTime that use % for format strings.
    
    The issue occurred because ChIdentifierPreparer inherited the
    _double_percents flag from SQLAlchemy's IdentifierPreparer, which
    is set to True for 'format' and 'pyformat' paramstyles. However,
    ClickHouse doesn't use % for parameter placeholders, so this
    doubling is inappropriate.
    
    This fix overrides the __init__ method to explicitly set
    _double_percents to False, preventing unwanted percent escaping.

--- clickhouse_connect/cc_sqlalchemy/sql/preparer.py
+++ clickhouse_connect/cc_sqlalchemy/sql/preparer.py
@@ -7,5 +7,12 @@ class ChIdentifierPreparer(IdentifierPreparer):
 
     quote_identifier = staticmethod(quote_identifier)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Disable percent doubling for ClickHouse
+        # ClickHouse doesn't use % for parameter placeholders, so doubling
+        # breaks string literals containing % (e.g., formatDateTime format strings)
+        self._double_percents = False
+
     def _requires_quotes(self, _value):
         return True
