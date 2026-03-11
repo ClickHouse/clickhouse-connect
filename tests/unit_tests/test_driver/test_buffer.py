161aa3e7c34bb5855dbcb31d0075df4c814cfff3
Joe S <joe.spadola@clickhouse.com>
2026-03-11 11:03:03 -0700

    reenable and fix driverc drift

--- tests/unit_tests/test_driver/test_buffer.py
+++ tests/unit_tests/test_driver/test_buffer.py
@@ -1,5 +1,9 @@
+import pytest
+
 from clickhouse_connect.driver.buffer import ResponseBuffer as PyResponseBuffer
+from clickhouse_connect.driver.exceptions import StreamFailureError
 from clickhouse_connect.driver.exceptions import StreamCompleteException
+from clickhouse_connect.driver.transform import NativeTransform
 from clickhouse_connect.driverc.buffer import ResponseBuffer as CResponseBuffer  # pylint: disable=no-name-in-module
 from tests.helpers import bytes_source, to_bytes
 
@@ -37,3 +41,34 @@ def test_read_bytes():
             buff.read_bytes(10)
         except StreamCompleteException:
             pass
+
+
+def test_fixed_string_strips_padding():
+    data = bytes.fromhex("41 00 00 00 42 43 00 00")
+    expected = ["A", "BC"]
+    for cls in CResponseBuffer, PyResponseBuffer:
+        buff = bytes_source(data, cls=cls)
+        assert list(buff.read_fixed_str_col(4, 2, "utf8")) == expected
+
+
+def test_tagged_exception_extracts_clean_message():
+    exception_tag = "PU1FNUFH98"
+    response_body = (
+        b"bodybodybodybody\r\n"
+        b"__exception__PU1FNUFH98\r\n"
+        b"Big bam occurred right while reading the data\r\n"
+        b"46 PU1FNUFH98__exception__\r\n"
+    )
+
+    class TaggedSource:
+        def __init__(self):
+            self.gen = iter([response_body])
+            self.exception_tag = exception_tag
+
+        def close(self, ex: Exception | None = None):
+            pass
+
+    for cls in CResponseBuffer, PyResponseBuffer:
+        with pytest.raises(StreamFailureError) as ex:
+            NativeTransform.parse_response(cls(TaggedSource()))
+        assert str(ex.value) == "Big bam occurred right while reading the data"
---
+++ tests/unit_tests/test_driver/test_ctypes.py
@@ -0,0 +1,46 @@
+import json
+import os
+import subprocess
+import sys
+
+
+def runtime_state(use_c: str | None = None):
+    env = os.environ.copy()
+    if use_c is None:
+        env.pop("CLICKHOUSE_CONNECT_USE_C", None)
+    else:
+        env["CLICKHOUSE_CONNECT_USE_C"] = use_c
+    result = subprocess.run(
+        [sys.executable, "-c", """
+import json
+from clickhouse_connect.driver.httpclient import RespBuffCls
+from clickhouse_connect.datatypes.numeric import data_conv
+from clickhouse_connect.datatypes.temporal import numpy_conv
+print(json.dumps({
+    "buffer": RespBuffCls.__module__,
+    "data_conv": data_conv.__name__,
+    "numpy_conv": numpy_conv.__name__,
+}))
+"""],
+        capture_output=True,
+        check=True,
+        env=env,
+        text=True,
+    )
+    return json.loads(result.stdout)
+
+
+def test_runtime_hook_enables_c_modules_by_default():
+    assert runtime_state() == {
+        "buffer": "clickhouse_connect.driverc.buffer",
+        "data_conv": "clickhouse_connect.driverc.dataconv",
+        "numpy_conv": "clickhouse_connect.driverc.npconv",
+    }
+
+
+def test_runtime_hook_can_be_disabled():
+    assert runtime_state("0") == {
+        "buffer": "clickhouse_connect.driver.buffer",
+        "data_conv": "clickhouse_connect.driver.dataconv",
+        "numpy_conv": "clickhouse_connect.driver.npconv",
+    }
