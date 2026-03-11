323b76809580854a84c543691ab7c9d976fdad49
Joe Spadola <joe.spadola@clickhouse.com>
2026-03-11 16:21:34 -0700

    Reenable default Cython runtime path and reestablish C/Python parity (#679)
    * reenable and fix driverc drift
    
    * target ctypes directly
    
    * linting
    
    * update changelog
    
    * pivot cython optimization
    
    * update changelog

--- tests/unit_tests/test_driver/test_exception_extraction.py
+++ tests/unit_tests/test_driver/test_exception_extraction.py
@@ -2,6 +2,7 @@ from clickhouse_connect.driver.transform import (
     extract_error_message,
     extract_exception_with_tag,
 )
+from tests.helpers import TAGGED_EXCEPTION_TAG, TAGGED_EXCEPTION_BODY
 
 
 # pylint: disable=no-self-use
@@ -10,20 +11,9 @@ class TestExceptionExtraction:
 
     def test_extract_exception_with_tag_basic(self):
         """Test extracting a simple exception with the new format"""
-        exception_tag = "PU1FNUFH98"
-        error_msg = "Big bam occurred right while reading the data"
-
-        # Format: __exception__<TAG>\r\n<error message>\r\n<message_length> <TAG>__exception__\r\n
-        response_body = (
-            b"bodybodybodybody\r\n"
-            b"__exception__PU1FNUFH98\r\n"
-            b"Big bam occurred right while reading the data\r\n"
-            b"46 PU1FNUFH98__exception__\r\n"
-        )
-
-        result = extract_exception_with_tag(response_body, exception_tag)
+        result = extract_exception_with_tag(TAGGED_EXCEPTION_BODY, TAGGED_EXCEPTION_TAG)
         assert result is not None
-        assert error_msg in result
+        assert "Big bam occurred right while reading the data" in result
 
     def test_extract_exception_with_tag_multiline_error(self):
         """Test extracting an exception with multiple lines in the error message"""
