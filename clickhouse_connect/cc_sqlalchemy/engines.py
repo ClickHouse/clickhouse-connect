506174ea522ab9df845e2f9690258a68f783388f
Joe S <joe.spadola@clickhouse.com>
2026-03-19 14:02:42 -0700

    engine shim

---
+++ clickhouse_connect/cc_sqlalchemy/engines.py
@@ -0,0 +1,3 @@
+# pylint: disable=wildcard-import, unused-wildcard-import
+from clickhouse_connect.cc_sqlalchemy.ddl.tableengine import *
+from clickhouse_connect.cc_sqlalchemy.ddl.tableengine import __all__
