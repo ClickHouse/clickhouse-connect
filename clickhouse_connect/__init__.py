fdc0a890953507ae6bcf18a5a4f1db6c7ca23d65
Joe S <joe.spadola@clickhouse.com>
2026-04-22 11:15:24 -0700

    add test, reorder ver check

--- clickhouse_connect/__init__.py
+++ clickhouse_connect/__init__.py
@@ -1,11 +1,10 @@
 import sys
 
-from clickhouse_connect._version import version as __version__
-from clickhouse_connect.driver import create_async_client, create_client
-
 if sys.version_info < (3, 10):  # noqa: UP036
     raise RuntimeError("clickhouse-connect 1.0+ requires Python 3.10 or later. Python 3.9 users should pin to clickhouse-connect<1.0.")
 
+from clickhouse_connect._version import version as __version__
+from clickhouse_connect.driver import create_async_client, create_client
 
 __all__ = ["__version__", "driver_name", "get_client", "get_async_client"]
 
