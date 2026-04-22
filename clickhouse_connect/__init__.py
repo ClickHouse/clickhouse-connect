dae549156f7e0504ee67fd2b729a808ae3fff239
Joe S <joe.spadola@clickhouse.com>
2026-04-22 10:25:54 -0700

    add __all__ to init

--- clickhouse_connect/__init__.py
+++ clickhouse_connect/__init__.py
@@ -7,6 +7,8 @@ if sys.version_info < (3, 10):  # noqa: UP036
     raise RuntimeError("clickhouse-connect 1.0+ requires Python 3.10 or later. Python 3.9 users should pin to clickhouse-connect<1.0.")
 
 
+__all__ = ["__version__", "driver_name", "get_client", "get_async_client"]
+
 driver_name = "clickhousedb"
 
 get_client = create_client
