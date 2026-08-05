bc024373255d16955d56da9526f5a757deaefa53
Joe S <joe.spadola@clickhouse.com>
2026-08-05 10:01:18 -0700

    fix naive datetime insert semantics

--- clickhouse_connect/common.py
+++ clickhouse_connect/common.py
@@ -73,6 +73,7 @@ _init_common("autogenerate_query_id", (True, False), True)
 _init_common("dict_parameter_format", ("json", "map"), "json")
 _init_common("invalid_setting_action", ("send", "drop", "error"), "error")
 _init_common("naive_datetime_binding", ("wall", "legacy"), "wall")
+_init_common("naive_datetime_insert", ("local", "server"), "local")
 _init_common("max_connection_age", (), 10 * 60)  # Max time in seconds to keep reusing a database TCP connection
 _init_common("product_name", (), "")  # Product name used as part of client identification for ClickHouse query_log
 _init_common("readonly", (0, 1), 0)  # Implied "read_only" ClickHouse settings for versions prior to 19.17
